f4ed0073bb517984eb5e7574c9b30961a4bdec5c
Devin Pavao <dpavao@elevenwinds.com>
2017-03-03 14:32:01 -0500

    Added startup file to display product info
    This file also contains information about the console window such as the
    colour of the background and foreground. Any misc data to be done to the
    console's immediate launch window should be here.

---
+++ com.yppiti.FileCommandProcessor/Functions/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using com.yppiti.FileCommandProcessor;
+using com.yppiti.FileCommandProcessor.Processes.Interpreter;
+using com.yppiti.FileCommandProcessor.Processes.Interpreter.DTOs;
+
+namespace com.yppiti.FileCommandProcessor.Functions
+{
+    class Startup
+    {
+        public static void Command()
+        {
+            Scriptlet_Startup();
+        }
+
+        protected static void Scriptlet_Startup()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Clear();
+            string OpenStream = ("\nCommand+ File Processor\nCreated by Devin Pavao\nVersion 1.4\nGithub: https://github.com/Yppiti/CommandPlus\n=============================================");
+            CommandInterpreter.WriteToScreen(OpenStream);
+        }
+    }
+}
